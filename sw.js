969d759362866a8f4cc5dee2814a06d39a713e66
matteospagnolo96 <matteo.spagnolo96@gmail.com>
2026-02-28 20:32:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,19 @@
-const CACHE_NAME = 'habit-v3';
+const CACHE_NAME = 'habit-tracker-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
+  'index.html',
   'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-// Installazione: salva i file nella cache
-self.addEventListener('install', e => {
+// Installazione e salvataggio file in cache
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Attivazione: pulisce vecchia cache
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// Strategia: Cache prima, poi Rete
-self.addEventListener('fetch', e => {
+// Recupero file dalla cache (funzionamento offline)
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
