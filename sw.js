adc2e3ba43c34972e12fa2846de8257275b69fb7
matteospagnolo96 <matteo.spagnolo96@gmail.com>
2026-03-31 18:41:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'habit-v3';
+const CACHE_NAME = 'habit-v4';
 const OFFLINE_URL = './';
 
 self.addEventListener('install', e => {
@@ -28,6 +28,17 @@ self.addEventListener('fetch', e => {
                     return caches.match(OFFLINE_URL);
                 }
             });
+        })
+    );
+});
+
+// Pulisci le vecchie cache quando si attiva la nuova versione
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
         })
     );
 });
